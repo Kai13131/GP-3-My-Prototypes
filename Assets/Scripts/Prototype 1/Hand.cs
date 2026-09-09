aae67f8df0e217c33389d845ac4af1b7896823d8
Kai13131 <haiyang.he13@gmail.com>
2026-09-08 23:33:28 -0400

    3

---
+++ Assets/Scripts/Prototype 1/Hand.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 direction = (mousePosition - transform.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
